b7e0167403bfd73488bad341ec907663c0351cea
vakiko <victoria.c6140@gmail.com>
2022-08-14 17:34:19 -0500

    Day 23: Crossy Road game with moving cars & increasing difficulty

--- day23CrossyRoad/player.py
+++ day23CrossyRoad/player.py
@@ -1,7 +1,36 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+    def moveUp(self):
+        self.setheading(UP)
+        self.forward(MOVE_DISTANCE)
+    def moveDown(self):
+        self.setheading(DOWN)
+        self.forward(MOVE_DISTANCE)
+    def moveLeft(self):
+        self.setheading(LEFT)
+        self.forward(MOVE_DISTANCE)
+    def moveRight(self):
+        self.setheading(RIGHT)
+        self.forward(MOVE_DISTANCE)
+    def newLevel(self):
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
