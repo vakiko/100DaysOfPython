93df16131d435ce7680ad6399b984ae21dbf7b0f
vakiko <victoria.c6140@gmail.com>
2022-10-20 09:39:40 -0500

    removed venv files w gitignore

--- day45webScraper/main.py
+++ day45webScraper/main.py
@@ -1,16 +1,28 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", encoding="utf8") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+article_tag = soup.find(name="a", class_="hnuser")
+article_text = article_tag.getText()
+print(article_text)
+#article_link = article
+#article_upvote = 
+
+
+# with open("website.html", encoding="utf8") as file:
+#     contents = file.read()
     
-soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
-print(soup.title.name)
-print(soup.prettify())
-all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.prettify())
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
 
-section_heading = soup.find(name="h3", class_="heading")
+# section_heading = soup.find(name="h3", class_="heading")
 
-test = soup.select("ul li")
-print(test)
+# test = soup.select("ul li")
+# print(test)
